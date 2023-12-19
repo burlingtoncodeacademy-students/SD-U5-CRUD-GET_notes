67431dbab9d0bf592afc96f3a5d1b5c4756b98a3
Eric Winebrenner <ejwinebrenner@gmail.com>
2023-12-19 14:27:47 -0500

    completed notes updated

--- controllers/movie.controller.js
+++ controllers/movie.controller.js
@@ -32,28 +32,70 @@ router.post('/', async(req,res) => {
     }
 })
 
-//TODO Get All
+//Get All
 router.get("/", async(req,res) => {
     try {
         
+        const getAllMovies = await Movie.find();
+
+        getAllMovies.length > 0 ?
+            res.status(200).json({
+                result: getAllMovies
+            }) :
+            res.status(404).json({
+                result: `No movies found`
+            });
+
     } catch (err) {
         errorResponse(res,err);
     }
 })
 
-//TODO GET One
-router.get("/", async(req,res) => {
+//GET One
+router.get("/find-one/:id", async(req,res) => {
     try {
         
+        const { id } = req.params;
+        const getMovie = await Movie.findOne({_id: id})
+
+        getMovie ?
+            res.status(200).json({
+                result: getMovie
+            }) :
+            res.status(404).json({
+                result: "No movie found"
+            })
+
     } catch (err) {
         errorResponse(res,err);
     }
 })
 
 //TODO Get All by Genre
-router.get("/", async(req,res) => {
+router.get("/genre/:genre", async(req,res) => {
     try {
         
+        const { genre } = req.params;
+        let buildWord;
+
+        if(genre) {
+            for(let i = 0; i < genre.length; i++) {
+                i === 0 ? 
+                    buildWord = genre[i].toUpperCase() :
+                    buildWord += genre[i].toLowerCase()
+            }
+        }
+
+        const getMovies = await Movie.find({genre: buildWord});
+
+        getMovies.length > 0 ?
+            res.status(200).json({
+                result: getMovies
+            }) :
+            res.status(404).json({
+                result: 'No movies found'
+            });
+
     } catch (err) {
         errorResponse(res,err);
     }
